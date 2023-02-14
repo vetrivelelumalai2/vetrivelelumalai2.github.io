41199eb93ed216dfc2a4f1282f73a015bb463f60
Prasanth S <prasanth.snk@gmail.com>
2023-02-14 16:30:51 +0530

    changes

---
+++ sample.js
@@ -0,0 +1,58 @@
+function getData() {
+    return Promise.all([
+        fetch(atob("aHR0cHM6Ly9pcHR2LW9yZy5naXRodWIuaW8vaXB0di9sYW5ndWFnZXMvdGFtLm0zdQ==")).then(resp => resp.text()),
+        fetch(atob("aHR0cHM6Ly9wcmFzYW50aC1zbmsuZ2l0aHViLmlvL2lwdHYvdGFtaWxMb2NhbC5tM3U=")).then(resp => resp.text()),
+        fetch(atob("aHR0cHM6Ly9wcmFzYW50aC1zbmsuZ2l0aHViLmlvL3lvdXR1YmVfbTN1L3RhbWlsLm0zdQ==")).then(resp => resp.text())
+    ]).then(allResponses => {
+        return allResponses;
+    }).catch(err => {
+        return err;
+    });
+}
+
+
+function getData() {
+    return Promise.all([
+        fetch(atob("aHR0cHM6Ly9wcmFzYW50aC1zbmsuZ2l0aHViLmlvL3lvdXR1YmVfbTN1L21hbGF5YWxhbS5tM3U=")).then(resp => resp.text()),
+        fetch(atob("aHR0cHM6Ly9pcHR2LW9yZy5naXRodWIuaW8vaXB0di9sYW5ndWFnZXMvbWFsLm0zdQ==")).then(resp => resp.text()),
+        fetch(atob("aHR0cHM6Ly9wcmFzYW50aC1zbmsuZ2l0aHViLmlvL2lwdHYvbWFsYXlhbGFtLm0zdQ==")).then(resp => resp.text())
+    ]).then(allResponses => {
+        return allResponses;
+    }).catch(err => {
+        return err;
+    });
+}
+
+
+function getData() {
+    return fetch(atob("aHR0cHM6Ly9pcHR2LW9yZy5naXRodWIuaW8vaXB0di9pbmRleC5sYW5ndWFnZS5tM3U="))
+        .then(resp => resp.text())
+        .then(allResponses => {
+            return allResponses;
+        }).catch(err => {
+            return err;
+        });
+}
+
+function getData() {
+    return fetch(atob("aHR0cHM6Ly9pcHR2LW9yZy5naXRodWIuaW8vaXB0di9jYXRlZ29yaWVzL3h4eC5tM3U="))
+        .then(resp => resp.text())
+        .then(allResponses => {
+            return allResponses;
+        }).catch(err => {
+            return err;
+        });
+}
+
+function getData() {
+    return Promise.all([
+        fetch(atob("aHR0cHM6Ly9pcHR2LW9yZy5naXRodWIuaW8vaXB0di9jYXRlZ29yaWVzL3h4eC5tM3U=")).then(resp => resp.text()),
+        fetch(atob("aHR0cHM6Ly9pcHR2LW9yZy5naXRodWIuaW8vaXB0di9sYW5ndWFnZXMvbWFsLm0zdQ==")).then(resp => resp.text()),
+        fetch(atob("aHR0cHM6Ly9wcmFzYW50aC1zbmsuZ2l0aHViLmlvL2lwdHYvbWFsYXlhbGFtLm0zdQ==")).then(resp => resp.text())
+    ]).then(allResponses => {
+        return allResponses;
+    }).catch(err => {
+        return err;
+    });
+}
+
