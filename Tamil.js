097d96e278b2340c7b1357a2923c39ec4f2f3fe6
vetrivelelumalai2 <68172178+vetrivelelumalai2@users.noreply.github.com>
2023-02-19 23:33:16 +0100

    Create Tamil.js

---
+++ Tamil.js
@@ -0,0 +1,68 @@
+function _0x2c3b(_0x2bed5d, _0x1d6e53) {
+    var _0x36cb72 = _0x36cb();
+    return (
+        (_0x2c3b = function (_0x2c3b9d, _0x5b4b3b) {
+            _0x2c3b9d = _0x2c3b9d - 0xe4;
+            var _0x5385b5 = _0x36cb72[_0x2c3b9d];
+            return _0x5385b5;
+        }),
+        _0x2c3b(_0x2bed5d, _0x1d6e53)
+    );
+}
+function _0x36cb() {
+    var _0x5570e8 = [
+        "1109894bHEctz",
+        "3cXFMhD",
+        "2287510uluncw",
+        "text",
+        "463432wDwhyN",
+        "1188882PWoqhJ",
+        "1338741iAcAoZ",
+        "aHR0cHM6Ly9wcmFzYW50aC1zbmsuZ2l0aHViLmlvL2lwdHYvdGFtaWxfaXB0di5tM3U=",
+        "aHR0cHM6Ly9wcmFzYW50aC1zbmsuZ2l0aHViLmlvL2lwdHYvdGFtaWxfc2F0ZWxsaXRlX3R2Lm0zdQ==",
+        "35wylfDK",
+        "19016btMzio",
+        "then",
+        "102291qKtSYm",
+        "670PuWGIb",
+    ];
+    _0x36cb = function () {
+        return _0x5570e8;
+    };
+    return _0x36cb();
+}
+(function (_0x3c9038, _0x4a89b5) {
+    var _0xa671cd = _0x2c3b,
+        _0xaa2df2 = _0x3c9038();
+    while (!![]) {
+        try {
+            var _0x5abbf0 =
+                parseInt(_0xa671cd(0xe6)) / 0x1 +
+                (-parseInt(_0xa671cd(0xe8)) / 0x2) * (parseInt(_0xa671cd(0xe9)) / 0x3) +
+                (-parseInt(_0xa671cd(0xe4)) / 0x4) * (-parseInt(_0xa671cd(0xe7)) / 0x5) +
+                -parseInt(_0xa671cd(0xed)) / 0x6 +
+                (parseInt(_0xa671cd(0xf1)) / 0x7) * (parseInt(_0xa671cd(0xec)) / 0x8) +
+                -parseInt(_0xa671cd(0xee)) / 0x9 +
+                parseInt(_0xa671cd(0xea)) / 0xa;
+            if (_0x5abbf0 === _0x4a89b5) break;
+            else _0xaa2df2["push"](_0xaa2df2["shift"]());
+        } catch (_0x4bd206) {
+            _0xaa2df2["push"](_0xaa2df2["shift"]());
+        }
+    }
+})(_0x36cb, 0x56e28);
+function getData() {
+    var _0x389038 = _0x2c3b;
+    return Promise["all"]([
+        fetch(atob("aHR0cHM6Ly9wcmFzYW50aC1zbmsuZ2l0aHViLmlvL2lwdHYvdGFtaWxMb2NhbC5tM3U="))[_0x389038(0xe5)]((_0x336d86) => _0x336d86["text"]()),
+        fetch(atob("aHR0cHM6Ly9wcmFzYW50aC1zbmsuZ2l0aHViLmlvL3lvdXR1YmVfbTN1L3RhbWlsLm0zdQ=="))[_0x389038(0xe5)]((_0x4464b6) => _0x4464b6[_0x389038(0xeb)]()),
+        fetch(atob(_0x389038(0xf0)))[_0x389038(0xe5)]((_0x20ec2c) => _0x20ec2c[_0x389038(0xeb)]()),
+        fetch(atob(_0x389038(0xef)))[_0x389038(0xe5)]((_0xa2923c) => _0xa2923c[_0x389038(0xeb)]()),
+    ])
+        ["then"]((_0x373895) => {
+            return _0x373895;
+        })
+        ["catch"]((_0x9401f0) => {
+            return _0x9401f0;
+        });
+}
